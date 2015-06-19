f24d36b1fc6118843af4ba5b8fd592d3a57e47e4
Luiz Felippe S. Rodrigues <luiz.rodrigues@newcastle.ac.uk>
2015-06-19 17:51:58 +0100

    Separated constants and input switches from main f90 file. Updated launch scripts accordingly.

---
+++ Current/program/CE_010/Cosmic_evol/constants.f90
@@ -0,0 +1,93 @@
+! Contains math constants and unit conversions
+
+module math_constants
+
+  implicit none
+
+  double precision, parameter :: pi=     3.14156295358d0
+  double precision, parameter :: mp_g=   1.67262158d-24  !proton mass in grams
+  double precision, parameter :: cm_kpc= 3.08567758d21  !number of cm in 1 kpc
+  double precision, parameter :: km_kpc= 3.08567758d16  !number of km in 1 kpc
+  double precision, parameter :: cm_km=  1.d5  !number of cm in 1 km
+  double precision, parameter :: s_Gyr=  1.d9*365.25d0*24*3600  !number of s in 1 Gyr
+  double precision, parameter :: mkG_G=  1.d6  !number of mkG in 1 G
+
+end module math_constants
+
+
+module units  !Specifies code units
+  use math_constants
+
+  implicit none
+
+  ! DIMENSIONLESS UNITS
+  double precision, parameter :: etat0=  1.0d0
+  double precision, parameter :: h0=     1.0d0
+  double precision, parameter :: r_disk= 1.0d0 ! Note that radii have different
+                                               ! length units than other quantities
+  double precision, parameter :: B0=     1.0d0
+  double precision, parameter :: t0=     h0**2/etat0
+  double precision, parameter :: n0=     B0**2/h0**2*t0**2
+
+  ! DIMENSIONAL UNITS
+  ! Chosen radius at which to specify param values
+  ! (e.g. radius of solar neighbourhood)
+  double precision, parameter :: r_sol_kpc=    8.0d0
+  ! Unit of etat, corresp to a typical value, in km*kpc/s
+  double precision, parameter :: etat0_kmskpc= 1.d0/3*0.1d0*10.d0
+  ! Unit of etat, corresp to a typical value, in cm^2/s
+  double precision, parameter :: etat0_cm2s=   etat0_kmskpc*cm_km*cm_kpc
+  ! Unit of length, corresp to a typical half-disk thickness, in kpc
+  double precision, parameter :: h0_kpc=       0.5d0
+  ! Unit of length, corresp to a typical half-disk thickness, in km
+  double precision, parameter :: h0_km=h0_kpc*km_kpc
+  ! Unit of length, corresp to a typical half-disk thickness, in cm
+  double precision, parameter :: h0_cm=h0_km*cm_km
+  ! Unit of magnetic field, in mkG
+  double precision, parameter :: B0_mkG=       1.0d0
+  ! Unit of time, corresp to a typical vert diff time, in s*kpc/km
+  double precision, parameter :: t0_kpcskm= h0_kpc**2/etat0_kmskpc
+  ! Unit of time in Gyr
+  double precision, parameter :: t0_Gyr=t0_kpcskm/s_Gyr*km_kpc
+  ! Unit of time in s
+  double precision, parameter :: t0_s=t0_kpcskm*km_kpc
+  ! Unit of number density, in cm^-3
+  double precision, parameter :: n0_cm3= (B0_mkG/mkG_G)**2/h0_cm**2*t0_s**2/mp_g
+
+end module units
+
+
+module input_constants  ! Contains physical, model-dependent, constants
+
+  use math_constants
+  implicit none
+  ! TURBULENCE
+  !Ratio of tau (eddy turnover time) to correlation time of the turbulence
+  double precision, parameter :: ctau= 1.d0
+  ! ADVECTION NO-z
+  !No-z correction term for terms involving Uz*Br, Uz*Bp
+  double precision, parameter :: C_U=0.25d0
+  !No-z correction term for terms involving Uz*alp_m
+  double precision, parameter :: C_a=1.d0!0.3d0
+  ! SEED FIELD
+  !Only relevant if Rand_seed=F
+  double precision, parameter :: r1_kpc= 15.0d0
+  !Seed field amplitude as a fraction of equipartition magnetic field strength
+  double precision, parameter :: frac_seed= 0.01d0
+  integer, parameter :: nn=2 !Only relevant if Rand_seed=F
+  ! DIFFUSIVE MAGNETIC HELICITY FLUX
+  !No-z correction term for terms involving diffusive flux
+  double precision, parameter :: C_d= -pi**2
+  ! RESISTIVITY
+  !1.e-5 !Inverse magnetic Reynolds number
+  double precision, parameter :: Rm_inv= 0.d0
+  ! ALPHA EFFECT
+  !Factor determining strength of alpha effect
+  double precision, parameter :: C_alp=   1.0d0
+   !Relevant only if module Alp_ceiling=1; Limits maximum value of alpha to ceiling*v_kms
+  double precision, parameter :: alpceil= 0.5d0
+  ! RANDOM MAGNETIC FIELD
+  !Strength of the rms random magnetic field brms in units of Beq
+  double precision, parameter :: fmag= 0.5d0
+
+end module input_constants
---
+++ Current/program/CE_010/Cosmic_evol/parameters.f90
@@ -0,0 +1,81 @@
+! Contains switches (to be edited before any particular run)
+! (At some point we may substitute this by a function which reads parameter files...)
+
+module modules
+
+  implicit none
+
+  ! PARAMETER INPUTS
+  !Set to T to read in parameters from file, set to F to use defaults
+  logical, parameter :: Read_param= .true.
+
+  ! PARAMETER INPUTS
+  ! Set to T to read in parameters at several timesteps; set to F to read in parameters at the start only
+  logical, parameter :: Time_evol= .true.
+
+  ! ALPHA QUENCHING
+  ! Works with alg_quench=F; Set to T for dynamical quenching (d_alpha_m/dt eqn incl in sim)
+  logical, parameter :: Dyn_quench= .true.
+  ! Works with dyn_quench=F; Set to T for algebraic alpha quenching; F for no quenching
+  logical, parameter :: Alg_quench= .false.
+
+  ! RANDOM MAGNETIC FIELD
+  ! Strength of the rms random magnetic field brms in units of Beq
+  logical, parameter :: Floor= .true.
+
+  ! CLOSURE APPROXIMATION
+  ! Set to T for FOSA, F for minimal tau approximation
+  logical, parameter :: Damp= .false.
+
+  ! OUTFLOW VELOCITY
+  ! Set to T to make Uz decrease with radius; set to F to keep Uz constant with radius
+  logical, parameter :: Var_Uz= .false.
+
+  ! TURBULENT SCALE
+  ! Set to T to make l decrease with radius; set to F to keep l constant with radius
+  logical, parameter :: Var_l= .false.
+
+  ! TURBULENT VELOCITY
+  ! Set to T to make v decrease with radius; set to F to keep v constant with radius
+  logical, parameter :: Var_v= .false.
+
+  ! DENSITY
+  ! Set to T to make n decrease with radius; set to F to keep n constant with radius (Beq^2=4pi*n*mp*v^2)
+  logical, parameter :: Var_n= .true.
+
+  ! SCALE HEIGHT
+  ! Set to T to include variable scale height.
+  logical, parameter :: Flaring= .true.
+
+  ! SEED MAGNETIC FIELD
+  ! Set to F for random seed magnetic field, T for some other seed
+  logical, parameter :: Rand_seed= .false.
+
+  ! CEILING ON ALPHA EFFECT
+  ! Set to T to put a ceiling for alpha at alpceil*v
+  logical, parameter :: Alp_ceiling= .true.
+
+  ! BRANDT ROTATION CURVE
+  ! Set to T to use a Brandt rotation curve
+  logical, parameter :: Om_Brandt = .true.
+
+  ! ALPHA^2 EFFECT
+  ! Set to T to include alpha^2 effect; set to F to use alpha-omega approximation equations
+  logical, parameter :: Alp_squared= .true.
+
+  ! KRAUSE'S LAW
+  ! Set to T for alpha effect to decrease in proportion to omega (Krause's formula)
+  logical, parameter :: Krause= .true.
+
+  ! OMEGA EFFECT
+  ! Set to T to include Omega effect in dynamo, 0 for alpha^2 dynamo
+  logical, parameter :: Shear= .true.
+
+  ! ADVECTION
+  ! Set to F to turn off advection of the magnetic field
+  logical, parameter :: Advect= .true.
+
+  ! TURBULENT DIFFUSION
+  logical, parameter :: Turb_dif= .true.  !Set to F to turn off turbulent diffusion
+
+end module modules

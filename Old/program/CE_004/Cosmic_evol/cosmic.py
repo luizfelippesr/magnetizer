11fcfa5f40a64d13694b982b5f3ed6424b79c6a2
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2016-02-12 15:40:49 +0000

    Reorganizes repository.

--- Old/program/CE_004/Cosmic_evol/cosmic.py
+++
@@ -1,6 +0,0 @@
-
-
-import cosmic_evol.pycall as pycall
-
-PYTCH_INFO=0
-psim_true,dummy=pycall.dynamo.dynamo_run(PYTCH_INFO,FLAG,)
--- Old/program/CE_006/s.py
+++
@@ -1,5 +0,0 @@
-#import Cosmic_evol.read as r
-#import Cosmic_evol.plot as p
-#from Cosmic_evol.read import *
-from read import *
-#from plot import *

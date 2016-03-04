248e72400ab846919acfa4251ac6122a2c07b380
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2016-03-04 11:02:01 +0200

    Fixes the rotation curve for the bulge.

---
+++ source/rotationCurves.f90
@@ -0,0 +1,162 @@
+module rotationCurves
+  implicit none
+  private
+
+  public :: disk_rotation_curve, bulge_rotation_curve, halo_rotation_curve
+
+contains
+  subroutine disk_rotation_curve(r, r_disk, rmin, v_disk, Omega, Shear)
+    ! Computes the rotation curve associated with an exponential disk
+    ! Input:  r -> the radii where the rotation curve will be computed
+    !         r_disk ->  the half mass radius of the disk
+    !         v_disk -> the circular velocity at r_disk
+    !         r_min -> the minimum radius to be considered
+    ! Output: Omega -> angular velocity profile
+    !         Shear  -> shear profile
+    ! Info:   V^2 \propto y^2 [I0(y)K0(y)-I1(y)K1(y)] where y=r/r_s
+    !         r_s --> the scale radius
+    !         For the shear, see  http://is.gd/MnDKyS
+    ! Ref:    Binney & Tremaine or  Mo, Bosch & White
+
+    use Bessel_Functions
+    use input_constants
+    implicit none
+    double precision, intent(in) :: r_disk, v_disk, rmin
+    double precision, dimension(:), intent(in)  :: r
+    double precision, dimension(size(r)) :: A
+    double precision, dimension(size(r)),intent(out) :: Omega, Shear
+    double precision, parameter :: TOO_SMALL=2e-7 ! chosen empirically
+    double precision, parameter :: rs_to_r50=constDiskScaleToHalfMassRatio
+    double precision, dimension(size(r)) :: y
+    double precision :: rs, y50
+    integer :: i
+
+    ! Scale radius
+    rs = r_disk*constDiskScaleToHalfMassRatio
+    ! (Half-mass radius) / (scale radius)
+    y50 = 1d0/constDiskScaleToHalfMassRatio
+
+    ! Traps disks of negligible size
+    if (r_disk < rmin) then
+      Shear = 0
+      Omega = 0
+      return
+    end if
+
+    ! Sets y=r/rs, trapping r~0 (which breaks the evaluation
+    ! of the Bessel functions)
+    where (r > TOO_SMALL)
+      ! The absolute value is taken to deal with the ghost zone
+      y = abs(r) / rs
+    elsewhere
+      y = TOO_SMALL / rs
+    endwhere
+
+    do i=1,size(r)
+      A(i) = (  I0(y(i)) * K0(y(i))            &
+              - I1(y(i)) * K1(y(i)) )          &
+              ! Check this!
+            / ( I0(y50) * K0(y50)  &
+              - I1(y50) * K1(y50))
+      A(i) = sqrt(A(i))
+    end do
+    Omega = A*v_disk/r_disk
+
+    do i=1,size(r)
+      Shear(i) =    I1(y(i)) * K0(y(i))                       &
+                  - I0(y(i)) * K1(y(i))                       &
+                  -0.5d0 * K1(y(i)) *( I0(y(i)) + I2(y(i)) )  &
+                  +0.5d0 * I1(y(i)) *( K0(y(i)) + K2(y(i)) )
+    end do
+
+    Shear = Shear/A/2d0*v_disk/r_disk
+    return
+  end subroutine disk_rotation_curve
+
+  subroutine bulge_rotation_curve(r, r_bulge, v_bulge, Omega, Shear)
+    ! Computes the rotation curve associated with an Hernquist profile
+    ! Input:  r -> the radii where the rotation curve will be computed
+    !         r_bulge ->  the half mass radius of the spheroid
+    !         v_bulge -> the circular velocity at r_bulge
+    ! Output: Omega -> angular velocity profile
+    !         Shear  -> shear profile
+    ! Info:   V^2 \propto y/(y+1)^2  where y=r/a
+    ! Ref:    http://adsabs.harvard.edu/abs/1990ApJ...356..359H
+
+    implicit none
+    double precision, intent(in) :: r_bulge, v_bulge
+    double precision, dimension(:), intent(in)  :: r
+    double precision, dimension(size(r)) :: v, dvdr
+    double precision, dimension(size(r)),intent(out) :: Omega, Shear
+    double precision, parameter :: a_to_r50 = (sqrt(2.0d0)-1.0d0)
+    double precision, dimension(size(r)) :: y, a
+    double precision :: Constant, y50
+
+    ! Traps and deals with bulge-less galaxies
+    if (v_bulge<1e-3) then
+      Omega = 0d0
+      Shear = 0d0
+      return
+    endif
+
+    ! Computes characteristic radius
+    a = a_to_r50*r_bulge
+    y = abs(r)/a
+    y50 = 1d0/a_to_r50 ! r_bulge/a
+
+    Constant = v_bulge * (1d0 + y50) / sqrt(y50)
+
+    ! v =  (y/a_to_r50) * (a_to_r50 +1d0)**2 * (y +1d0)**(-2)
+    ! v = v_bulge * sqrt(v)
+    v = Constant * sqrt(y)/(y+1d0)
+
+    Omega = v/abs(r)
+
+    dvdr = Constant/r_bulge * ( 0.5d0/sqrt(y)/(y+1d0) - sqrt(y)/(y+1d0)**2 )
+!     dvdr = Constant/2d0/((y+1d0)*sqrt(y)) - Constant*sqrt(y)/(1d0+y)**2
+
+    Shear = dvdr - Omega
+
+  end subroutine bulge_rotation_curve
+
+  subroutine halo_rotation_curve(r, r_halo, v_halo, nfw_cs1, Omega, Shear)
+    ! Computes the rotation curve associated with an NFW halo
+    ! Warning: This ignores effects of adiabatic contraction!
+    !          It need to be accounted for later
+    ! Input: r -> the radii where the rotation curve will be computed
+    !        r_halo -> the virial radius of the halo
+    !        v_halo -> the circular velocity at r_halo
+    !        nfw_cs1 -> 1/c_s -> inverse of the NFW concentration parameter
+    !                        i.e. (NFW scale radius)/(virial radius)
+    ! Output: Omega -> angular velocity profile
+    !         Shear  -> shear profile
+    ! Info:  V^2 \propto {ln[(cs1+y)/cs1] - y/(cs1+y)} /
+    !                    {ln[(cs1+1)/cs1] - 1/(cs1+1)} / y
+    ! Ref: NFW profile
+
+    implicit none
+    double precision, intent(in) :: r_halo, v_halo, nfw_cs1
+    double precision, dimension(:), intent(in)  :: r
+    double precision, dimension(size(r)), intent(out) :: Omega, Shear
+    double precision, dimension(size(r)) :: v, dv
+    double precision, dimension(size(r)) :: y
+    double precision, dimension(size(r)) :: B
+    double precision :: A
+
+    y = abs(r) / r_halo
+
+    A = 1.0 / ( log((nfw_cs1+1d0)/nfw_cs1) - 1d0/(nfw_cs1+1d0) )
+    A = sqrt(A)
+
+    B = (log((nfw_cs1+y)/nfw_cs1) - y/(nfw_cs1+y)) / y
+    v = A * v_halo * sqrt(B)
+
+    dv = 1.0/(nfw_cs1+y)**2-(log((nfw_cs1+y)/nfw_cs1)-y/(nfw_cs1+y))/y**2/2.0
+    dv = dv * A * v_halo
+
+    Omega = v/r
+    Shear = dv - Omega
+
+  end subroutine halo_rotation_curve
+
+end module rotationCurves

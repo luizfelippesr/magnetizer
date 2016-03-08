c9c8d7c534671c25b6a8bc1765f336e83542e6a0
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2016-03-08 15:42:28 +0200

    Improves the output module: makes it more readable; includes missing units.

--- source/output.f90
+++ source/output.f90
@@ -18,52 +18,96 @@ contains
     call IO_start_galaxy(gal_id, info)
     if (info>2) print *, 'Galaxy',gal_id,' -> IO initialised'
     
-    call IO_write_dataset('t', gal_id, info,ts_t_Gyr, units='Gyr', &
+    call IO_write_dataset('t', gal_id, info,                     &
+                          ts_t_Gyr,                              &
+                          units='Gyr',                           &
                           description='Time since the Big Bang.')
-    call IO_write_dataset('Br', gal_id, info,ts_Br(:,:) * B0_mkG / B0, &
+
+    call IO_write_dataset('Br', gal_id, info,                    &
+                          ts_Br(:,:) * B0_mkG / B0,              &
                           units='microgauss')
-    call IO_write_dataset('Bp', gal_id, info,ts_Bp(:,:) * B0_mkG / B0, &
+
+    call IO_write_dataset('Bp', gal_id, info,                    &
+                          ts_Bp(:,:) * B0_mkG / B0,              &
                           units='microgauss')
+
     if (Dyn_quench) then
-      call IO_write_dataset('alp_m', gal_id, info,ts_alp_m(:,:))
+      call IO_write_dataset('alp_m', gal_id, info,               &
+                            ts_alp_m(:,:)*h0_km/h0/t0_s*t0 ,     &
+                            units='km/s')
     endif
-    call IO_write_dataset('Bzmod', gal_id, info,ts_Bzmod(:,:) * B0_mkG / B0, &
+
+    call IO_write_dataset('Bzmod', gal_id, info,                 &
+                          ts_Bzmod(:,:) * B0_mkG / B0,           &
                           units='microgauss')
-    call IO_write_dataset('h', gal_id, info, ts_h(:,:)/h0*h0_kpc*1d3, &
-                          units='pc', description='Disk scaleheight')
-    call IO_write_dataset('Omega',gal_id,info, &
-                          ts_om(:,:)*h0_km/h0_kpc/t0_s*t0, &
-                          units='km/s/kpc', description='Angular velocity')
-    call IO_write_dataset('Shear', gal_id, &
-                          info,ts_G(:,:)*h0_km/h0_kpc/t0_s*t0,&
-                          units='km/s/kpc', description='Shear')
-    call IO_write_dataset('l', gal_id, info,ts_l(:,:)/h0*h0_kpc*1d3, &
-                          units='pc', description='Turbulent length')
+
+    call IO_write_dataset('h', gal_id, info,                     &
+                          ts_h(:,:)/h0*h0_kpc*1d3,               &
+                          units='pc',                            &
+                          description='Disk scaleheight')
+
+    call IO_write_dataset('Omega',gal_id,info,                   &
+                          ts_om(:,:)*h0_km/h0_kpc/t0_s*t0,       &
+                          units='km/s/kpc',                      &
+                          description='Angular velocity')
+
+    call IO_write_dataset('Shear', gal_id, info,                 &
+                          ts_G(:,:)*h0_km/h0_kpc/t0_s*t0,        &
+                          units='km/s/kpc',                      &
+                          description='Shear')
+
+    call IO_write_dataset('l', gal_id, info,                     &
+                          ts_l(:,:)/h0*h0_kpc*1d3,               &
+                          units='pc',                            &
+                          description='Turbulent length')
 !     call IO_write_dataset('v', gal_id, info, &
 !                           ts_v(:,:)*h0_km/h0/t0_s*t0, &
 !                           units='km/s', description='Turbulent velocity')
-    call IO_write_dataset('etat', gal_id, info,ts_etat(:,:))
+
+    call IO_write_dataset('etat', gal_id, info,                     &
+                          ts_etat(:,:)*h0_km*h0_kpc/h0**2/t0_s*t0,  &
+                          units='kpc km/s')
+
     call IO_write_dataset('tau', gal_id, info,ts_tau(:,:))
-    call IO_write_dataset('alp_k', gal_id, info,ts_alp_k(:,:))
-    call IO_write_dataset('Uz', gal_id, info, &
-                          ts_Uz(:,:)*h0_km/h0/t0_s*t0, &
-                          units='km/s', &
+
+    call IO_write_dataset('alp_k', gal_id, info,                    &
+                          ts_alp_k(:,:)*h0_km/h0/t0_s*t0 ,          &
+                          units='km/s')
+
+    call IO_write_dataset('Uz', gal_id, info,                       &
+                          ts_Uz(:,:)*h0_km/h0/t0_s*t0,              &
+                          units='km/s',                             &
                           description='Vertical velocity (outflow)')
+
 !     call IO_write_dataset('Ur', gal_id, info,ts_Ur(:,:)*h0_km/h0/t0_s*t0, &
 !                           units='km/s', description='Radial velocity')
-    call IO_write_dataset('n', gal_id, info,ts_n(:,:)*n0_cm3/n0, &
-                          units='cm^-3', &
+
+    call IO_write_dataset('n', gal_id, info,                        &
+                          ts_n(:,:)*n0_cm3/n0,                      &
+                          units='cm^-3',                            &
                           description='Number density of diffuse gas')
-    call IO_write_dataset('Beq', gal_id, info, ts_Beq(:,:)* B0_mkG / B0, &
-                          units='microgauss', &
+
+    call IO_write_dataset('Beq', gal_id, info,                      &
+                          ts_Beq(:,:)* B0_mkG / B0,                 &
+                          units='microgauss',                       &
                           description='Equipartition field')
-    call IO_write_dataset('rmax', gal_id, info,ts_rmax/h0*h0_kpc, &
-                          units='kpc', description='Radius of maximum Bp')
+
+    call IO_write_dataset('rmax', gal_id, info,                     &
+                          ts_rmax/h0*h0_kpc,                        &
+                          units='kpc',                              &
+                          description='Radius of maximum B_phi')
+
     call IO_write_dataset('delta_r', gal_id, info,ts_delta_r)
-    call IO_write_dataset('alp', gal_id, info,ts_alp(:,:))
+
+    call IO_write_dataset('alp', gal_id, info,                      &
+                          ts_alp(:,:)*h0_km/h0/t0_s*t0 ,            &
+                          units='km/s')
     call IO_write_dataset('dt', gal_id, info,ts_dt)
-    call IO_write_dataset('r', gal_id, info, ts_rkpc, &
-                          units='kpc', description='Radius')
+
+    call IO_write_dataset('r', gal_id, info,                        &
+                          ts_rkpc,                                  &
+                          units='kpc',                              &
+                          description='Radius')
     
     if (info>2) print *, 'Galaxy',gal_id,' -> datasets written'
 

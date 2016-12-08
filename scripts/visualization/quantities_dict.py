4b066a750bad8f302e48b0f20956b6b6c04ba54f
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2016-12-08 17:27:29 +0000

    Includes seed of a formating "quantities_dict"ionary.

---
+++ scripts/visualization/quantities_dict.py
@@ -0,0 +1,16 @@
+"""
+Contains a convenience dictionary relating the code quantities names
+with their LaTeX representation as well as units.
+
+Structure:
+    quantities_dic = { code-name : ( LaTeX-name, LaTeX-units )}
+
+Note: LaTeX-name and LaTeX units assume they are in the math mode!
+"""
+
+
+quantities_dict = {
+  'h' : ( r'h', r'{\rm pc}')
+  'n' : ( r'n', r'{\rm cm}^{-3}')
+  }
+

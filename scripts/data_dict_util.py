4cf704b1ba0070f416b0f5f0d16dd20341b90f38
Luiz Felippe S. Rodrigues <luiz.rodrigues@newcastle.ac.uk>
2016-12-13 17:21:12 +0000

    Changes data_dict_tools.

--- scripts/data_dict_util.py
+++ scripts/data_dict_util.py
@@ -1,20 +1,24 @@
 """ Useful routines to deal with SAM_data dictionaries """
 
-def filter_dict(data_dict, ok, ignore_list=[]):
-    """ Filters a dictionary full of numpy (or similar) arrays using 'ok' 
+def filter_dict(ok, data_dict, ignore_list=[]):
+    """
+    Filters a dictionary full of numpy (or similar) arrays using 'ok'
     filter array.
     
     Returns a new up-to-date dictionary
     """
     new_dict = dict()
+
     for key in data_dict:
         if key not in ignore_list:
             new_dict[key] = data_dict[key][ok]
+
     return new_dict
 
 
 def filter_dictionary_inplace(ok, dictionary, ignore_list=[]):
-    """ Filters a dictionary full of numpy (or similar) arrays using 'ok' 
+    """
+    Filters a dictionary full of numpy (or similar) arrays using 'ok'
     filter array.
     
     NB the dictionary is altered in the process. 
@@ -24,3 +28,16 @@ def filter_dictionary_inplace(ok, dictionary, ignore_list=[]):
             dictionary[k] = dictionary[k][ok]
     return
 
+def dictionary_concatenate_inplace(dict_a, dict_b):
+    """
+    Concatenates all the keys of two dictionaries.
+    """
+    import numpy as np
+    keys = dict_a.keys()
+    for k in keys:
+        if k not in dict_b:
+            print 'Deleting key: ', k
+            del dict_a[k]
+        else:
+            dict_a[k] = np.concatenate((dict_a[k],dict_b[k]))
+    return

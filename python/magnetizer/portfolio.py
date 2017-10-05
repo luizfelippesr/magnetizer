15434b743e6857c3320456aa17f8bb49b4cba029
Luiz Felippe S. Rodrigues <luiz.rodrigues@newcastle.ac.uk>
2017-10-05 17:51:50 +0100

    Refactored single_galaxy_portfolio function.

--- python/magnetizer/portfolio.py
+++ python/magnetizer/portfolio.py
@@ -14,6 +14,32 @@ from extra_quantities import compute_extra_quantity
 
 units_dict = {}
 
+default_quantities = [
+                        'Omega',
+                        'Shear',
+                        'Uz',
+                        'alp',
+                        #'alp_k',
+                        #'alp_m',
+                        #'delta_r',
+                        'etat',
+                        'h',
+                        #'l',
+                        'n',
+                        #'tau',
+                        'Beq',
+                        'Bp',
+                        'Br',
+                        'Bzmod',
+                        'Btot',
+                        #'Bfloor',
+                        #'growth',
+                        #'Dcrit',
+                        #'D_Dc',
+                        #'P',
+                        #'P2',
+                        ]
+
 formatted_units_dict = {'microgauss':r'\mu{{\rm G}}',
               'cm^-3':r'{{\rm cm}}^{{-3}}',
               'pc':r'{{\rm pc}}',
@@ -42,6 +68,10 @@ quantities_dict = {'Bp'   : r'\overline{{B}}_\phi',
                    'D_Dc': r'D/D_c',
                    '|Bp|'   : r'|\overline{{B}}_\phi|',
                    'Bpmod'   : r'|\overline{{B}}_\phi|',
+                   'Mstars_bulge': r'$M_{{\star,bulge}}',
+                   'Mstars_disk': r'$M_{{\star,disc}}',
+                   'Mgas_disk': r'$M_{{gas,disc}}',
+                   'r_disk': r'$r_{{disc}}',
                    }
 
 log_quantities = ('Beq','n','h')
@@ -65,16 +95,79 @@ def plot_frame(ts, rs, quantity, name='', cmap=plt.cm.YlGnBu,
 
         ax.plot(r, q,color=color, rasterized=True, **args)
 
-        ax.set_xlabel(r'$r\,[\rm kpc]$')
 
-        ax.set_xlim([0.0, r.base.max()])
 
         # Some advanced formating
         if name in quantities_dict:
             name = quantities_dict[name]
-        ax.set_ylabel(r'$ {0} {1} $'.format(name,unit))
-        ax.grid(alpha=0.2)
 
-        if name in log_quantities:
-          ax.set_yscale('log')
+    ax.set_xlim([0.0, rs.base[rs.base>0].max()])
+    ax.set_xlabel(r'$r\,[\rm kpc]$')
+    ax.set_ylabel(r'$ {0} {1} $'.format(name,unit))
+    ax.grid(alpha=0.2)
+    if name in log_quantities:
+        ax.set_yscale('log')
+
+
+def single_galaxy_portfolio(igal, run_obj, nrows=5, ncols=3,
+                            selected_quantities=None, cmap=plt.cm.viridis):
+    if selected_quantities is None:
+        # Copies (do not copy reference) the default list of quantities
+        selected_quantities = list(default_quantities)
+    else:
+        # Ensures correct type
+        selected_quantities = list(selected_quantities)
+
+    # Reads all the selected galaxy properties (and redshift variations)
+    prop_dict = {}
+    for name in (selected_quantities +
+                 ['Mstars_disk','Mgas_disk','Mstars_bulge','r_disk','r']):
+        prop_dict[name] = run_obj.get_galaxy(name, igal)
+
+    # Get galaxy properties closest to the present day and prepares
+    # a string with the info
+    info = ''
+    ok = np.isfinite(prop_dict['Mstars_disk']) # Avoids NaNs
+    for q in ('r_disk', 'Mstars_bulge', 'Mstars_disk','Mgas_disk'):
+        val = prop_dict[q][ok][-1].base[()] # Ignores units
+        if q == 'r_disk':
+            val = r'{0:.2f} \,\rm kpc'.format(val)
+        else:
+            if val==0:
+                continue
+            val = r'{0:.2g}\times10^{{{1}}}\rm M_\odot'.format(
+                val/10**np.floor(np.log10(val)),
+                int(np.floor(np.log10(val))))
+        info += r' $-$  {0} = {1}$'.format(quantities_dict[q], val)
+
+    # Prepares the figure
+    fig = plt.figure(figsize=(8.268,11.69),dpi=300)
+    subplot_idx = 0
+
+    # Plots each of the quantities
+    for quantity in selected_quantities:
+        subplot_idx += 1
+        if subplot_idx > nrows*ncols:
+            break
+        ax = fig.add_subplot(nrows, ncols, subplot_idx)
+
+        plot_frame(run_obj.times, prop_dict['r'],
+                   prop_dict[quantity], name=quantity,ax=ax,
+                   cmap=cmap, linewidth=1.5, alpha=0.5)
+
+
+    # Adds title
+    fig.suptitle('Galaxy {0}{1}'.format(igal, info))
+    # Finds space for the color bar
+    fig.tight_layout()
+    fig.subplots_adjust(top=0.95, bottom=0.1)
+    # Prepares colorbar
+    norm = matplotlib.colors.Normalize(vmin=run_obj.times[ok].min(),
+                                       vmax=run_obj.times[ok].max())
+    ax = fig.add_axes([.065, .04, .9, .01])
+    x = matplotlib.colorbar.ColorbarBase(ax, cmap=plt.cm.viridis, norm=norm,
+                                    orientation='horizontal')
+    x.set_label(r'$t\,\,[{{\rm Gyr }}]$')
+
+    return fig
 

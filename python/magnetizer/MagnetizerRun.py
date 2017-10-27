8286e2839d3da23e14396c1011947cb71d9fa925
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2017-10-28 00:26:19 +0530

    Multiple output files for a single magnetizerRun object. It is now possible to pass a list of output file paths (and, consistently, a list of input file paths) to generate a single MagnetizerRun object. This allows, for instance, one to use multiple ivolumes of the Millenium simulation at the same time and plot everything with the previous machinery. This also allows to incrementing the statistics cumulatively, by generating various Magnetizer input files from the same galform input. TODO: the get_galaxy method needs to be adapted accordingly.

--- python/magnetizer/MagnetizerRun.py
+++ python/magnetizer/MagnetizerRun.py
@@ -14,41 +14,63 @@ class MagnetizerRun(object):
     input_file_path -> path hdf5 file containing Magnetizer input data.
                        If absent, will assume the same as output_file_path.
     """
-    def __init__(self, output_file_path, input_file_path=None,
+    def __init__(self, output_path, input_path=None,
                  z_tolerance=0.01, verbose=False):
 
-        # Open files for reading
-        hout = h5py.File(output_file_path,'r')
-        if input_file_path is not None:
-            hin = h5py.File(input_file_path,'r')
-        else:
-            hin = hout
+        if isinstance(output_path, basestring):
+            output_path = [output_path]
+        if isinstance(input_path, basestring):
+            input_path = [input_path]
 
-        # Construct a dictionary of references to the hdf5 files
+        # Open files for reading
+        houts = [h5py.File(path,'r') for path in output_path]
+
+        if input_path is not None:
+            hins = [h5py.File(path,'r') for path in input_path]
+        else:
+            hins = houts
+
+        # Construct a dictionaries of references to the hdf5 files
         # (merges all groups...)
-        data_dict = {x: hout['Output'][x] for x in hout['Output']}
-        data_dict.update({x: hin['Input'][x]
-                              for x in hin['Input']})
-        data_dict.update({x: hout['Log'][x]
-                              for x in hout['Log']})
+        self._data = []
+        for hout, hin in zip(houts, hins):
+            data_dict = {x: hout['Output'][x] for x in hout['Output']}
+            data_dict.update({x: hin['Input'][x] for x in hin['Input']})
+            data_dict.update({x: hout['Log'][x] for x in hout['Log']})
+            self._data.append(data_dict)
+
         self.verbose = verbose
-        self._data = data_dict
-        self._hin = hin
-        self._hout = hout
-        self.redshifts = self._data['z'][:]
-        self.times = self._data['t'][:]
-        self.parameters = Parameters(hout)
+        self._hin = hins
+        self._hout = houts
+        # Uses the first hdf5 as reference
+        self.redshifts = self._data[0]['z'][:]
+        self.times = self._data[0]['t'][:]
+        self.parameters = Parameters(houts[0])
         self._cache = {}
+        self._galaxies_cache = {}
         self.name = self.parameters.name
         self._z_tolerance = z_tolerance
 
-        self.ngals, self.ngrid, self.nz = data_dict['h'].shape
+        self.ngals, self.ngrid, self.nz = self._data[0]['h'].shape
 
-        # Creates a mask for finished runs
-        self._completed = self._data['completed'][:]>0.
-        self._completed = self._completed[:,0] # Eliminates bogus axis
+        self.gal_id = []
+        self.ivol = []
+        self._completed = []
+        for i, data_dict in enumerate(self._data):
+            # Creates a masks for finished runs
+            completed = data_dict['completed'][:]>0.
+            completed = completed[:,0] # Eliminates bogus axis
+            self._completed.append(completed)
 
-        self.gal_id = np.arange(self._completed.size)[self._completed]
+            # Stores indices and ivolumes of all galaxies
+            gal_id = np.arange(completed.size)[completed]
+            ivol = np.ones_like(gal_id)*i # Later this will be substituted
+                                          # by the actual ivolume
+            self.gal_id.append(gal_id)
+            self.ivol.append(ivol)
+
+        self.gal_id = np.concatenate(self.gal_id)
+        self.ivol = np.concatenate(self.ivol)
 
         # Place-holders used elsewhere
         self._valid = None
@@ -56,7 +78,7 @@ class MagnetizerRun(object):
 
 
     def get(self, quantity, z, position=None, binning=None):
-        reload(eq)
+        #reload(eq)
         iz = self.__closest_redshift_idx(z)
         key = (quantity, iz)
 
@@ -66,11 +88,11 @@ class MagnetizerRun(object):
             if quantity == 'status':
                 self._cache[key] = status[self._completed, iz]
                 raise NotImplementedError
-            elif quantity in self._data:
-                profile = len(self._data[quantity].shape)==3
+            elif quantity in self._data[0]:
+                profile = len(self._data[0][quantity].shape)==3
 
-                if 'Units' in self._data[quantity].attrs:
-                    unit = self._data[quantity].attrs['Units']
+                if 'Units' in self._data[0][quantity].attrs:
+                    unit = self._data[0][quantity].attrs['Units']
                     if len(unit)==1:
                         unit = unit[0] # unpacks array..
                     unit = eq.units_dict[unit]
@@ -80,22 +102,25 @@ class MagnetizerRun(object):
                 if self.verbose:
                     print 'Loading {0} at z={1}'.format(quantity,
                                                         self.redshifts[iz])
-
-                self._cache[key] = self._clean(self._data[quantity],iz,
-                                                   profile)
+                data = []
+                for i, data_dict in enumerate(self._data):
+                      data.append(self._clean(data_dict[quantity],iz,i,profile))
             else:
                 if self.verbose:
                     print 'Computing {0} at z={1}'.format(quantity,
                                                           self.redshifts[iz])
-                new_data, unit = eq.compute_extra_quantity(quantity, self._data,
-                                                        select_z=iz,
-                                                        return_units=True)
+                data = []
+                for i, data_dict in enumerate(self._data):
+                    new_data, unit = eq.compute_extra_quantity(quantity,
+                                                               data_dict,
+                                                               select_z=iz,
+                                                               return_units=True)
+                    profile = len(new_data.shape)==2
 
-                profile = len(new_data.shape)==2
+                    data.append( self._clean(new_data, iz, i, profile,
+                                             pre_selected_z=True) )
 
-                self._cache[key] = self._clean(new_data, iz, profile,
-                                                   pre_selected_z=True)
-
+            self._cache[key] = np.concatenate(data)
             if unit is not None:
                 self._cache[key] = self._cache[key]*unit
 
@@ -164,42 +189,39 @@ class MagnetizerRun(object):
         return self._cache[key]
 
 
-    def _clean(self, dataset, iz = slice(None, None, None), profile=True,
+    def _clean(self, dataset, iz = slice(None), ivol=0, profile=True,
                pre_selected_z=False):
         """
         Removes incomplete and invalid data from a dataset.
         """
 
         if self._valid is None:
+            self._valid = {}
+
+        if ivol not in self._valid:
             # Pre-loads heights, which will be used to distinguish between valid
             # and invalid outputs.
-            self._valid = self._data['h'][self._completed, :, :] > 0
+            self._valid[ivol] = self._data[ivol]['h'][self._completed[ivol], :, :] > 0
 
+        valid = self._valid[ivol]
+        completed = self._completed[ivol]
 
         if profile:
             if pre_selected_z:
                 # If the redshift was previously selected
                 # and if corresponds to a profile
-                return np.where(self._valid[:,:,iz],
-                                dataset[self._completed,:],
-                                np.NaN)
+                return np.where(valid[:,:,iz], dataset[completed,:], np.NaN)
 
-            return np.where(self._valid[:,:,iz],
-                            dataset[self._completed,:,iz],
-                            np.NaN)
-
+            return np.where(valid[:,:,iz], dataset[completed,:,iz], np.NaN)
         else:
             # If doesn't correspond to a profile
             if pre_selected_z:
                 # If the redshift was previously selected
-                # and if corresponds to a profile
-                return np.where(self._valid[:,0,iz],
-                                dataset[self._completed],
-                                np.NaN)
+                # and if doesn't correspond to a profile
+                return np.where(valid[:,0,iz], dataset[completed], np.NaN)
 
-            return np.where(self._valid[:,0,iz],
-                                dataset[self._completed,iz],
-                                np.NaN)
+            return np.where(valid[:,0,iz], dataset[completed,iz], np.NaN)
+
 
     def _clean_gal(self, dataset, gal_id, profile=True,
                    pre_selected_gal_id=False):

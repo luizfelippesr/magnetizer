2351129ab15f26cadf8f0bf34098b223f6226ae5
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2017-12-21 17:59:47 +0000

    Includes GPL3 message in all python files This follows Free Software Foundation recommendation.

--- python/generate_galform_output_from_txt.py
+++ python/generate_galform_output_from_txt.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python
+# Copyright (C) 2018  Luiz Felippe S. Rodrigues, Luke Chamandy
+#
+# This file is part of Magnetizer.
+#
+# Magnetizer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Magnetizer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Magnetizer.  If not, see <http://www.gnu.org/licenses/>.
+#
 import numpy as N
 import h5py
 from hdf5_util import add_dataset

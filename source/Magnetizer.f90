fd87c539c2ca3eeee59f6608a88dc4c4eefc51f1
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2018-01-05 14:49:29 +0000

    Changes the name of main module file.

--- source/Magnetizer.f90
+++ source/Magnetizer.f90
@@ -1,6 +1,5 @@
+!# Magnetizer
 !# Copyright (C) 2018  Luiz Felippe S. Rodrigues, Luke Chamandy
-!#
-!# This file is part of Magnetizer.
 !#
 !# Magnetizer is free software: you can redistribute it and/or modify
 !# it under the terms of the GNU General Public License as published by

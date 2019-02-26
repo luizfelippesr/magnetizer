642358e3955a3c53bd7fef0de7b8ae9a36accf25
Luiz Felippe S. Rodrigues <luiz.rodrigues@ncl.ac.uk>
2019-02-26 10:49:59 +0000

    Includes random field in the emisivity calculation and removes LoSintegrate_all_redshift. Provisionally, the turbulent velocity is hard-coded. This needs to be changed later. LoSintegrate_all_redshift proved not really useful. I removed from the code to avoid the burden of maitaining it.

--- source/LoSintegrate_aux.f90
+++ source/LoSintegrate_aux.f90
@@ -22,7 +22,8 @@ module LoSintegrate_aux
     double precision :: wavelength = 20d-2 ! 20 cm or 1.49 GHz
     double precision :: alpha = 3d0
     double precision :: theta = 0d0
-    logical :: B_scale_with_z = .false.
+    logical :: B_scale_with_z = .true.
+    logical :: ignore_small_scale_field = .false.
     integer :: nz_points = 500
   end type
 
@@ -34,6 +35,7 @@ module LoSintegrate_aux
   contains
   subroutine LoSintegrate(props, impacty_rmax, impactz_rmax, data, &
                                        iz, RM_out, I_out)
+    use input_constants
     type(Galaxy_Properties), intent(in) :: props
     type(LoS_data), intent(inout) :: data
     ! Impact parameter in y- and z-directions in units of rmax
@@ -44,7 +46,7 @@ module LoSintegrate_aux
     logical, dimension(2*props%n_grid) :: valid
     logical :: lRM, lI
     double precision, allocatable, dimension(:) :: x_path, z_path
-    double precision, allocatable, dimension(:) :: Bpara, Bperp, ne, h
+    double precision, allocatable, dimension(:) :: Bpara, Bperp, Brnd, ne, h
     double precision, allocatable, dimension(:) :: z_path_new, x_path_new
     double precision :: Bx, By, Bz, rmax
     double precision :: tmp, impact_y, impact_z
@@ -185,12 +187,22 @@ module LoSintegrate_aux
       endwhere
     endif
 
+    allocate(Brnd(size(Bpara)))
+    if (data%ignore_small_scale_field) then
+      Brnd = ne*0d0
+    else
+      ! Computes the random field in microgauss, assuming hydrogen mass and:
+      !                       v0=10km/s f=0.5
+      Brnd = sqrt(4*pi*ne*Hmass)*10d3 * 0.5d0 * 1d6
+      ! TODO change this to the values used in the simulation
+    endif
+
     data%number_of_cells(iz) = data%number_of_cells(iz) + size(x_path)
     if (lI) then
       ! Synchrotron emission
       ! NB Using the total density as a proxy for cosmic ray electron density
       ! NB2 Increments previous calculation
-      data%Stokes_I(iz) = data%Stokes_I(iz)+Compute_Stokes_I(Bperp, ne,      &
+      data%Stokes_I(iz) = data%Stokes_I(iz)+Compute_Stokes_I(Bperp, Brnd, ne, &
                                                               x_path, z_path, &
                                                               data%wavelength,&
                                                               data%alpha)
@@ -203,182 +215,6 @@ module LoSintegrate_aux
 
   end subroutine LoSintegrate
 
-  subroutine LoSintegrate_all_redshifts(props, impacty_rmax, impactz_rmax, data, RM_out, I_out)
-    type(Galaxy_Properties), intent(in) :: props
-    type(LoS_data), intent(inout) :: data
-    ! Impact parameter in y- and z-directions in units of rmax
-    double precision, intent(in) :: impactz_rmax, impacty_rmax
-    logical, intent(in), optional :: RM_out, I_out
-    double precision,dimension(props%n_redshifts,2*props%n_grid) :: Bpara_all, &
-                                              Bperp_all, xc, zc, ne_all, h_all
-    logical, dimension(props%n_redshifts,2*props%n_grid) :: valid
-    logical :: lRM, lI
-    double precision, allocatable, dimension(:) :: x_path, z_path
-    double precision, allocatable, dimension(:) :: Bpara, Bperp, ne, h
-    double precision, allocatable, dimension(:) :: z_path_new, x_path_new
-    double precision, dimension(props%n_redshifts) :: Bx, By, Bz, rmax
-    double precision, dimension(props%n_redshifts) :: tmp, impact_y, impact_z
-    double precision :: zmin, zmax, xmin, xmax
-    integer, dimension(2*props%n_grid) :: js
-    integer :: i, j, it
-    logical, parameter :: ldense_z = .true.
-
-    ! Allocates/initialises everything
-    valid = .false.
-    if (.not.allocated(data%RM)) then
-      allocate(data%RM(props%n_redshifts))
-      allocate(data%Stokes_I(props%n_redshifts))
-      allocate(data%number_of_cells(props%n_redshifts))
-      data%RM = 0; data%number_of_cells = 0; data%Stokes_I = 0
-    endif
-    ! Skips if outside the galaxy
-    if (abs(impacty_rmax)>1) return
-
-    lRM = .false.; lI = .true. ! Default values
-    if (present(RM_out)) lRM = RM_out
-    if (present(I_out)) lI = I_out
-
-
-    rmax = props%Rcyl(:,props%n_grid)
-    impact_y = impacty_rmax * rmax
-    impact_z = impactz_rmax * rmax
-
-
-    ! Works on all redshifts simultaneously, looping over the radial grid-points
-    ! i -> index for quantities in a cartesian box
-    ! j -> index for axi-symmetric quantities
-    do i=1,2*props%n_grid
-      ! Constructs coordinates and auxiliary indices js
-      if (i<props%n_grid+1) then
-        ! Index for behind the x-z plane
-        j=props%n_grid-i+1
-        js(i) = j
-        ! sign for x coordinate
-        xc(:,i) = -1
-      else
-        ! Index ahead of the x-z plane
-        j = i-props%n_grid
-        js(i) = j
-        ! sign for x coordinate
-        xc(:,i) = 1
-      end if
-
-      ! The available values for x are x_i = sqrt(R_i^2-b^2)
-      tmp = props%Rcyl(:,j)**2-impact_y(:)**2 ! NB allocated on-the-fly: Fortran2003 feature
-      where (tmp>=0)
-        xc(:,i) = xc(:,i) * sqrt(tmp)
-        ! Stores a mask to be used with the Fortran 2003 'pack' intrinsic function
-        valid(:,i) = .true.
-      endwhere
-
-      ! Sets z-coord
-      zc(:,i) = xc(:,i) / tan(data%theta) + impact_z(:)
-
-      ! Bx = Br * x/R - Bp * y/R
-      Bx = props%Br(:,j) * xc(:,i)/props%Rcyl(:,j) - props%Bp(:,j)*impact_y(:)/props%Rcyl(:,j)
-      ! By = Br * y/R + Bp * x/R
-      By = props%Br(:,j) * impact_y(:)/props%Rcyl(:,j) + props%Bp(:,j)* xc(:,i)/props%Rcyl(:,j)
-      ! Bz = Bzmod * sign(z)
-      Bz = sign(props%Bz(:,j), zc(:,i))
-
-      ! Stores density and scale height
-      ! NB the scaling with of n with z will be done later!
-      ne_all(:,i) = props%n(:,j)
-      h_all(:,i) = props%h(:,j)
-
-      ! Simple vector calculations
-
-      ! B_\parallel = dot(B,n), where n is the LoS direction
-      ! [NB  n = (sin(theta),0,cos(theta))  ]
-      Bpara_all(:,i) = Bx*sin(data%theta) + Bz*cos(data%theta)
-      ! B_\perp = \sqrt{ (B_x - B_\parallel\sin\theta)^2 + B_y^2 +
-      !                  + (B_z -B_\parallel\cos\theta)^2 }
-      Bperp_all(:,i) = ( Bx - Bpara_all(:,i)*sin(data%theta) )**2   &
-                         + By**2 +                                  &
-                       ( Bz - Bpara_all(:,i)*cos(data%theta) )**2
-      Bperp_all(:,i) = sqrt(Bperp_all(:,i))
-    enddo
-
-    ! Now work is done for each redshift (as the valid section of each array may
-    ! may be different)
-    do it=1,props%n_redshifts
-      ! Skips empty cells
-      if (all(.not.valid(it,:))) cycle
-
-      ! Filters away invalid parts of the arrays
-      Bpara = pack(Bpara_all(it,:),valid(it,:))
-      Bperp = pack(Bperp_all(it,:),valid(it,:))
-      ne = pack(ne_all(it,:),valid(it,:))
-      h = pack(h_all(it,:),valid(it,:))
-      x_path = pack(xc(it,:),valid(it,:))
-      z_path = pack(zc(it,:),valid(it,:))
-
-      ! Skips (bogus) empty cells
-!       if (maxval(z_path)-minval(z_path)<1d-6) cycle
-
-      ! The following makes the grid denser, to allow meaningful z-integration
-      ! This step is irrelevant for edge-on, but is important for face-on
-      if (ldense_z) then
-        ! Sets a tentative z-maximum to 3.5 times the scale-height
-        zmin = -3.5d0*h(1); zmax = 3.5d0*h(size(h))
-
-        ! Computes corresponding values for x
-        xmin = (zmin - impact_z(it))*tan(data%theta)
-        xmax = (zmax - impact_z(it))*tan(data%theta)
-        ! Forces x coordinate to live within the original grid
-        if (abs(xmin) > abs(x_path(1))) then
-          xmin = x_path(1)
-          zmin = z_path(1)
-        endif
-        if (abs(xmax) > abs(x_path(size(x_path)))) then
-          xmax = x_path(size(x_path))
-          zmax = z_path(size(x_path))
-        endif
-
-        z_path_new = linspace(zmin,zmax, data%nz_points)
-        x_path_new = (z_path_new - impact_z(it))*tan(data%theta)
-
-        call densify(Bperp, x_path, x_path_new)
-        call densify(Bpara, x_path, x_path_new)
-        call densify(ne, x_path, x_path_new)
-        call densify(h, x_path, x_path_new)
-
-        z_path = z_path_new
-        x_path = x_path_new
-      endif
-      ! Adds the z dependence to the density
-      ne = ne  * exp(-abs(z_path)/h)
-      ! Adds the z dependence the magnetic field
-      if (data%B_scale_with_z) then
-        ! Scale with z (coordinate)
-        Bpara = Bpara * exp(-abs(z_path)/h)
-        Bperp = Bperp * exp(-abs(z_path)/h)
-      else
-        ! Constant for |z|<h, zero otherwise
-        where (abs(z_path)/h>1)
-          Bpara = 0d0
-          Bpara = 0d0
-          Bperp = 0d0
-        endwhere
-      endif
-
-      data%number_of_cells(it) = data%number_of_cells(it) + size(x_path)
-      if (lI) then
-        ! Synchrotron emission
-        ! NB Using the total density as a proxy for cosmic ray electron density
-        ! NB2 Increments previous calculation
-        data%Stokes_I(it) = data%Stokes_I(it)+Compute_Stokes_I(Bperp, ne,      &
-                                                               x_path, z_path, &
-                                                               data%wavelength,&
-                                                               data%alpha)
-      endif
-      if (lRM) then
-        ! Faraday rotation (backlit)
-        ! NB Using the total density as a proxy for thermal electron density
-        data%RM(it) = Compute_RM(Bpara, ne, x_path, z_path)
-      endif
-    enddo
-  end subroutine LoSintegrate_all_redshifts
 
   function IntegrateImage(props,data,iz,number_of_calls,method,error) result(res)
     use fgsl
@@ -482,7 +318,7 @@ module LoSintegrate_aux
     Compute_RM = Integrator(integrand, x_path, z_path)
   end function Compute_RM
 
-  pure function Compute_Stokes_I(Bperp, ncr, x_path, z_path, wavelength, alpha)
+  pure function Compute_Stokes_I(Bperp, Brnd, ncr, x_path, z_path, wavelength, alpha)
     ! Computes Stokes parameter I along a line of sight
     !
     ! Input: Bperp -> 1d-array, B parallel to the LoS, in microgauss
@@ -491,23 +327,23 @@ module LoSintegrate_aux
     !        alpha -> real, spectral index of the CR electrons, default: 3
     !        x_path,z_path -> 1d-array, positions along path, in kpc
     ! Output: total synchrotron emissivity in arbitrary units
-    double precision, dimension(:), intent(in) :: Bperp, ncr, x_path, z_path
+    double precision, dimension(:), intent(in) :: Bperp, Brnd, ncr, x_path, z_path
     double precision, dimension(size(ncr)) :: integrand
     double precision, intent(in) :: wavelength
     double precision, optional, intent(in) :: alpha
     double precision :: Compute_Stokes_I
 
     if (present(alpha)) then
-      integrand = emissivity(Bperp, ncr, wavelength, alpha)
+      integrand = emissivity(Bperp, Brnd, ncr, wavelength, alpha)
     else
-      integrand = emissivity(Bperp, ncr, wavelength)
+      integrand = emissivity(Bperp, Brnd, ncr, wavelength)
     endif
 
     Compute_Stokes_I = Integrator(integrand, x_path, z_path) * 1d6
 
   end function Compute_Stokes_I
 
-  pure function emissivity(Bperp, ncr, wavelength, alpha)
+  pure function emissivity(Bperp, Brnd, ncr, wavelength, alpha)
     ! Synchrotron emissivity
     !
     ! Input: Bperp -> 1d-array, magnitude of B perpendicular to LoS, in microgauss
@@ -516,10 +352,10 @@ module LoSintegrate_aux
     !        alpha -> real, spectral index of the CR electrons, default: 3
     ! Output: emissivity -> 1d-array, in arbitrary units
     !
-    double precision, dimension(:), intent(in) :: Bperp, ncr
+    double precision, dimension(:), intent(in) :: Bperp, Brnd, ncr
     double precision, intent(in) :: wavelength
     double precision, optional, intent(in) :: alpha
-    double precision, dimension(size(ncr)) :: emissivity
+    double precision, dimension(size(ncr)) :: emissivity, Bperp_tot2
     double precision :: aa
 
     if (present(alpha)) then
@@ -528,7 +364,8 @@ module LoSintegrate_aux
       aa = 3.0
     endif
 
-    emissivity = ncr * Bperp**((alpha+1.0)/2d0) * wavelength**((alpha-1.0)/2d0)
+    Bperp_tot2 = Bperp**2+Brnd**2
+    emissivity = ncr * Bperp_tot2**((alpha+1.0)/4d0) * wavelength**((alpha-1.0)/2d0)
 
   end function emissivity
 

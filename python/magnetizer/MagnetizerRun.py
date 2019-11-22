luizfelippesr/magnetizer

commit 477ae75b865a01d9745a1cccf8f271b5d9d83678

Makes cleaning of invalid runs more efficient Before, all redshifts were being loaded at the cleaning phase

python/magnetizer/MagnetizerRun.py
@@ -387,26 +387,29 @@ class MagnetizerRun(object):
             self._valid = {}
 
         if ivol not in self._valid:
+            self._valid[ivol]= {}
+
+        if iz not in self._valid[ivol]:
             # Pre-loads heights, which will be used to distinguish between valid
             # and invalid outputs.
-            self._valid[ivol]=self._data[ivol]['h'][self._completed[ivol],:,:]>0
+            self._valid[ivol][iz]=self._data[ivol]['h'][self._completed[ivol],:,iz]>0
 
-        valid = self._valid[ivol]
+        valid = self._valid[ivol][iz]
         completed = self._completed[ivol]
 
         if quantity_type =='profile':
             if pre_selected_z:
                 # If the redshift was previously selected
                 # and if corresponds to a profile
-                return np.where(valid[:,:,iz], dataset[completed,:], np.NaN)
+                return np.where(valid[:,:], dataset[completed,:], np.NaN)
 
             return np.where(valid[:,:,iz], dataset[completed,:,iz], np.NaN)
         elif quantity_type == 'multiple':
             if pre_selected_z:
-                output = [ np.where(valid[:,0,iz], dataset[completed,iq], np.NaN)
+                output = [ np.where(valid[:,0], dataset[completed,iq], np.NaN)
                           for iq in range(dataset.shape[-1])]
             else:
-                output = [ np.where(valid[:,0,iz], dataset[completed,iq,iz], np.NaN)
+                output = [ np.where(valid[:,0], dataset[completed,iq,iz], np.NaN)
                           for iq in range(dataset.shape[-2])]
             if len(output)==1:
                 output = output[0]
@@ -419,9 +422,9 @@ class MagnetizerRun(object):
             if pre_selected_z:
                 # If the redshift was previously selected
                 # and if doesn't correspond to a profile
-                return np.where(valid[:,0,iz], dataset[completed], np.NaN)
+                return np.where(valid[:,0], dataset[completed], np.NaN)
 
-            return np.where(valid[:,0,iz], dataset[completed,iz], np.NaN)
+            return np.where(valid[:,0], dataset[completed,iz], np.NaN)
 
 
     def _clean_gal(self, dataset, gal_id, ivol, quantity, profile=True,

commit dbdcdc465e874981957252d18bc27e983b3eb684

Corrects bug introduced in 477ae75b865a01d9745a1cccf8f271b5d9d83678.

python/magnetizer/MagnetizerRun.py
@@ -213,9 +213,9 @@ class MagnetizerRun(object):
                     quantity_type = 'multiple'
                 else:
                     if len(self._data[0][quantity].shape)==3:
-                      quantity_type = 'profile'
+                        quantity_type = 'profile'
                     else:
-                      quantity_type = 'scalar'
+                        quantity_type = 'scalar'
 
                 if 'Units' in self._data[0][quantity].attrs:
                     unit = self._data[0][quantity].attrs['Units']
@@ -230,7 +230,7 @@ class MagnetizerRun(object):
                                                         self.redshifts[iz])
                 data = []
                 for i, data_dict in enumerate(self._data):
-                      data.append(self._clean(data_dict[quantity],iz,i,quantity_type))
+                    data.append(self._clean(data_dict[quantity],iz,i,quantity_type))
                 if quantity_type == 'multiple':
                     result = np.concatenate(data, axis=1)
                 else:
@@ -402,8 +402,7 @@ class MagnetizerRun(object):
                 # If the redshift was previously selected
                 # and if corresponds to a profile
                 return np.where(valid[:,:], dataset[completed,:], np.NaN)
-
-            return np.where(valid[:,:,iz], dataset[completed,:,iz], np.NaN)
+            return np.where(valid[:,:], dataset[completed,:,iz], np.NaN)
         elif quantity_type == 'multiple':
             if pre_selected_z:
                 output = [ np.where(valid[:,0], dataset[completed,iq], np.NaN)
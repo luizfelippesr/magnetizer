ded3f63e023aad31269a84bdfcc5810a9371ddc5
Luiz Felippe S. Rodrigues <luizfelippesr@gmail.com>
2019-12-09 13:23:47 +0100

    Testing FRB module.

--- source/tests.FRB.f90
+++ source/tests.FRB.f90
@@ -1,8 +1,13 @@
 program testFRB
     use FRB
+    use tools, only: linspace
     implicit none
+    double precision, parameter :: pi=     3.14156295358d0
     integer, parameter :: nsamples=10000
+    integer, parameter :: npoints=1001
     double precision, dimension(3,nsamples) :: FRB_pos
+    double precision, dimension(npoints) :: x, y, z
+    double precision :: theta
     integer :: u, i
 
     ! The following will generate nsamples positions (x,y,z) of FRBs to check
@@ -18,6 +23,21 @@ program testFRB
                                         Mstars_disk=0d0, rmax=10d0)
     enddo
     open (newunit=u, file='testFRB_position.dat', status='new',form='unformatted')
+    write (u) FRB_pos
+    close (u)
+
+    ! The follwing will FRB positions along a particular LoS. The distribution
+    ! of positions should again follow the weighting by the molecular density.
+    z = linspace(-2.5d0,2.5d0,npoints)
+    y = 2.0
+    theta = 30.d0  * pi/180d0
+    x = z / tan(theta)
+
+    do i=1,nsamples
+        FRB_pos(:,i) = get_FRB_LoS_position(x,y(1),z,h_FRB=0.033d0, r_disk=3d0, Mgas_disk=1d13, &
+                                        Mstars_disk=0d0, rmax=10d0)
+    enddo
+    open (newunit=u, file='testFRB_LoS_position.dat', status='new',form='unformatted')
     write (u) FRB_pos
     close (u)
 
